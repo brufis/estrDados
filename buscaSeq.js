af02c7c7e86342fecdd6bde1ef36a1e47bf1637b
brufis <faleiros.b@gmail.com>
2020-08-31 12:18:37 +0000

    xxx

--- buscaSeq.js
+++ buscaSeq.js
@@ -1,3 +1,14 @@
+
+
+function buscaSeq(nomes, valorBusca) {
+    for(let i = 0; i < nomes.length; i++ ){
+        if nomes[i] === valorBusca) return i
+    }
+    
+    return -1 
+}
+
+
 // Importar um vetor com mais de 100k de nomes
 
 const nomes = require('./Dados/vetor-nomes')
@@ -6,4 +17,19 @@ const nomes = require('./Dados/vetor-nomes')
 
 console.time('BRUNA')
 console.log(buscaSeq(nomes, 'BRUNA'))
+console.timeEnd('BRUNA')
+
+// Nova busca em lista
+
+function buscaSeq2(listaNomes, valorBusca, fnComp) {
+    for(let i = 0; i = listaNomes.length; i++){
+     if(fnComp(listaNomes[i], valorBusca)) return i
+    }
+    return -1
+}
+
+const = listaNomes = require('./Dados/lista-nomes')
+
+console.time('BRUNA')
+console.log(buscaSeq(listaNomes, 'BRUNA'))
 console.timeEnd('BRUNA')
